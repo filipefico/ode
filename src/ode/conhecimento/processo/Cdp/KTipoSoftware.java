4304148997da930f244a46276d84a80e239fb12e
rsjuliao <rsjuliao@9a94fb9e-9367-8fd8-e54f-9d0b81a662fe>
2010-09-03 19:57:26 +0000

    Criação da classe KTipoSoftware.java

--- src/ode/conhecimento/processo/Cdp/KTipoSoftware.java
+++ src/ode/conhecimento/processo/Cdp/KTipoSoftware.java
@@ -1,25 +1,8 @@
 package ode.conhecimento.processo.Cdp;
 import ode.conhecimento.principal.Cdp.Conhecimento;
 
-
-
-/****************** Classe KTipoSoftware *************************/
-
-
-/** Representa os conhecimentos sobre os Tipos de Software do
- * ambiente ODE
- *
- *@hibernate.joined-subclass
- *      table = "conh_proc_ktiposoftware"
- *@hibernate.joined-subclass-key
- *      column = "id"
- */
-
 public class KTipoSoftware extends Conhecimento {
-  
-  /**
-	 * 
-	 */
+
 	private static final long serialVersionUID = -8750817165681213347L;
 
 /**Construtor.*/
