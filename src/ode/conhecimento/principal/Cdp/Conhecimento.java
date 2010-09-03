d144af7419cb88032b13d147961fc5723c6ca3d6
rsjuliao <rsjuliao@9a94fb9e-9367-8fd8-e54f-9d0b81a662fe>
2010-09-03 19:47:51 +0000

    Classe Conhecimento criada.

--- src/ode/conhecimento/principal/Cdp/Conhecimento.java
+++ src/ode/conhecimento/principal/Cdp/Conhecimento.java
@@ -3,79 +3,64 @@ package ode.conhecimento.principal.Cdp;
 import nucleo.comuns.persistencia.ObjetoPersistente;
 
 public class Conhecimento extends ObjetoPersistente {
-	/**
-	 * 
-	 */
+
 	private static final long serialVersionUID = -2477658761966661172L;
-	
+
 	/** Nome do conhecimento. */
 	private String nome;
 	/** Descricao do conhecimento. */
 	private String descricao;
+
 	/** Construtor. */
 	public Conhecimento() {
 	}
 
-	   /**
-     * Obtem nome do conhecimento.
-     * @hibernate.property length="100"
-     */
-    public String getNome() {
-        return nome;
-    }
-    
-    /**
-     * Atribui nome ao conhecimento.
-     */
-    public void setNome(String parNome) {
-        nome = parNome;
-    }
-    
-    /**
-     * Obtém descricao do conhecimento.
-     * @hibernate.property length="500"
-     */
-    public String getDescricao() {
-        return descricao;
-    }
-    
-    /**
-     * Atribui descricao ao conhecimento.
-     */
-    public void setDescricao(String parDescricao) {
-        descricao = parDescricao;
-    }
-    
-    /* Nao precisa mais.
-     * Obtém o repositório do conhecimento.
-     *#falta mapear
-     
-    public RepositorioConhecimento getRepositorio() {
-        return repositorio;
-    }
-     
-    /*
-     * Atribui o repositório do conhecimento.
-     
-    public void setRepositorio(RepositorioConhecimento parRepositorio) {
-        repositorio = parRepositorio;
-    }
-     */
-    
-    /** Descreve o objeto Conhecimento detalhadamente. */
-    public String sPubDescreverObjeto(){
-        StringBuffer locDadosConhecimento = new StringBuffer();
-        locDadosConhecimento.append(" *** CONHECIMENTO *** \n");
-        locDadosConhecimento.append(" - Nome: ");
-        locDadosConhecimento.append(getNome() +"\n");
-        locDadosConhecimento.append(" - Descrição: ");
-        locDadosConhecimento.append(getDescricao());
-        return locDadosConhecimento.toString();
-    }
-    
-    /** Sobreescreve toString(). */
-    public String toString(){
-        return this.nome;
-    }
-    
+	/**
+	 * Obtem nome do conhecimento.
+	 * 
+	 * @hibernate.property length="100"
+	 */
+	public String getNome() {
+		return nome;
+	}
+
+	/**
+	 * Atribui nome ao conhecimento.
+	 */
+	public void setNome(String parNome) {
+		nome = parNome;
+	}
+
+	/**
+	 * Obtém descricao do conhecimento.
+	 * 
+	 * @hibernate.property length="500"
+	 */
+	public String getDescricao() {
+		return descricao;
+	}
+
+	/**
+	 * Atribui descricao ao conhecimento.
+	 */
+	public void setDescricao(String parDescricao) {
+		descricao = parDescricao;
+	}
+
+	/** Descreve o objeto Conhecimento detalhadamente. */
+	public String sPubDescreverObjeto() {
+		StringBuffer locDadosConhecimento = new StringBuffer();
+		locDadosConhecimento.append(" *** CONHECIMENTO *** \n");
+		locDadosConhecimento.append(" - Nome: ");
+		locDadosConhecimento.append(getNome() + "\n");
+		locDadosConhecimento.append(" - Descri��o: ");
+		locDadosConhecimento.append(getDescricao());
+		return locDadosConhecimento.toString();
+	}
+
+	/** Sobreescreve toString(). */
+	public String toString() {
+		return this.nome;
+	}
+
 }
