dc5282e32fa8f9e752489745e415e2fd6deb1a52
rsjuliao <rsjuliao@9a94fb9e-9367-8fd8-e54f-9d0b81a662fe>
2010-09-03 19:50:21 +0000

    Criação da classe PessoaExemploDAOHibernate.

--- src/ode/exemplo2/pessoa/Cgd/PessoaExemploDAOHibernate.java
+++ src/ode/exemplo2/pessoa/Cgd/PessoaExemploDAOHibernate.java
@@ -3,16 +3,13 @@ package ode.exemplo2.pessoa.Cgd;
 import nucleo.comuns.persistencia.NucleoDAOBaseHibernate;
 import ode.exemplo2.pessoa.Cdp.PessoaExemplo;
 
-public class PessoaExemploDAOHibernate extends NucleoDAOBaseHibernate<PessoaExemplo> implements
-		PessoaExemploDAO {
+public class PessoaExemploDAOHibernate extends
+		NucleoDAOBaseHibernate<PessoaExemplo> implements PessoaExemploDAO {
 
 	@Override
 	protected Class getClasseDominio() {
 		// TODO Auto-generated method stub
 		return PessoaExemplo.class;
 	}
-	
-	
-	
 
 }
