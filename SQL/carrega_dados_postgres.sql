abfbca79ed27a96076a566ba85035b634d2da96e
alexandregncoelho <alexandregncoelho@gmail.com>
2011-07-14 21:22:18 +0000

    Alteração no script do banco.

--- SQL/carrega_dados_postgres.sql
+++ SQL/carrega_dados_postgres.sql
@@ -1,3 +1,37 @@
+INSERT INTO CONFIGURACOESSISTEMA(id, uuid, version) 
+			VALUES (1, '4586102c-1b82-4fd8-933a-13a46b2dc003' , 0);
+
+INSERT INTO HIBERNATE_SEQUENCES (SEQUENCE_NAME, SEQUENCE_NEXT_HI_VALUE ) 
+			VALUES  ('ConfiguracaoSistema', 2);
+
+INSERT INTO FUNCIONALIDADE(
+            id, uuid, version, nome, srcjanela, funcionalidadepai_id, configuracoes_sistema_id, 
+            pos, funcionalidade_id) 
+            VALUES (1, 'cf024718-356b-4844-a502-bf52acac8c81', 12, 'Administração', '', null, 1, 0, null);
+            
+INSERT INTO FUNCIONALIDADE(
+            id, uuid, version, nome, srcjanela, funcionalidadepai_id, configuracoes_sistema_id, 
+            pos, funcionalidade_id) 
+            VALUES (2, 'c544f9e64aa6-4aa9-b100-1590e71af8ed', 3, 'Pessoas', 'ode.controleProjeto.cci.CtrlProjetoCRUD', null, 1, 0, null);
+            
+INSERT INTO HIBERNATE_SEQUENCES (SEQUENCE_NAME, SEQUENCE_NEXT_HI_VALUE ) 
+			VALUES  ('Funcionalidade', 3);
+            
+INSERT INTO PERFILACESSO(
+            id, uuid, version, nome, descricao)
+    		VALUES (1 , '9010512e-ba0a-444a-83cb-048698e98404' , 4 , 'Administrador' , 'Administrador do Sistema');
+    		
+INSERT INTO HIBERNATE_SEQUENCES (SEQUENCE_NAME, SEQUENCE_NEXT_HI_VALUE ) 
+			VALUES  ('PerfilAcesso', 2);
+    		
+INSERT INTO PERFILACESSO_FUNCIONALIDADE(
+            perfilacesso_id, funcionalidadespermitidas_id)
+    		VALUES (1, 1);
+
+INSERT INTO PERFILACESSO_FUNCIONALIDADE(
+            perfilacesso_id, funcionalidadespermitidas_id)
+    		VALUES (1, 2);
+    		
 INSERT INTO RECURSOHUMANO (id, uuid, version, nome, ativo)
 VALUES (1, '9440682b-b0e8-4341-9179-bc623b89d083', 0, 'Ricardo de Almeida Falbo', true);
 INSERT INTO RECURSOHUMANO (id, uuid, version, nome, ativo)
@@ -5,10 +39,10 @@ VALUES (2, '6b42720e-5b31-4971-a055-e244430d957f', 0, 'Alexandre Coelho', true);
 INSERT INTO HIBERNATE_SEQUENCES (SEQUENCE_NAME, SEQUENCE_NEXT_HI_VALUE ) 
 VALUES  ('RECURSOHUMANO', 1);
 
-INSERT INTO NUCLEOUSERDETAILS (id, uuid, version, username, password, recursohumano_id)
-VALUES (1, '36ac7738-b3bc-40cc-98ab-21c5f59db410', 0, 'admin', 'e8d95a51f3af4a3b134bf6bb680a213a',1);
-INSERT INTO NUCLEOUSERDETAILS (id, uuid, version, username, password, recursohumano_id)
-VALUES (2, '45as7738-b3bc-40cc-98ab-21c5f59db410', 0, 'alexandre.coelho', 'e8d95a51f3af4a3b134bf6bb680a213a',2);
+INSERT INTO NUCLEOUSERDETAILS (id, uuid, version, username, password, perfilacesso_id, recursohumano_id)
+VALUES (1, '36ac7738-b3bc-40cc-98ab-21c5f59db410', 0, 'admin', 'e8d95a51f3af4a3b134bf6bb680a213a',1, 1);
+INSERT INTO NUCLEOUSERDETAILS (id, uuid, version, username, password, perfilacesso_id, recursohumano_id)
+VALUES (2, '45as7738-b3bc-40cc-98ab-21c5f59db410', 0, 'alexandre.coelho', 'e8d95a51f3af4a3b134bf6bb680a213a', 1, 2);
 INSERT INTO HIBERNATE_SEQUENCES (SEQUENCE_NAME, SEQUENCE_NEXT_HI_VALUE ) 
 VALUES  ('NucleoUserDetails', 1);
 INSERT INTO GRANTEDAUTHORITYIMPL (id, uuid, version, authority)
