a5bedc0e327dca0eff94f70e2e85291be54dd81f
alexandregncoelho <alexandregncoelho@gmail.com>
2011-07-15 19:28:19 +0000

    Autenticação e validação de usuários funcionando.

--- SQL/carrega_dados_postgres.sql
+++ SQL/carrega_dados_postgres.sql
@@ -1,75 +1,172 @@
+---------------------------------------------------------------------------------------------
+-- Configuracoes do Sistema : Usada para recuperar as funcionalidades disponíveis no ODE  
+---------------------------------------------------------------------------------------------
+
 INSERT INTO CONFIGURACOESSISTEMA(id, uuid, version) 
-			VALUES (1, '4586102c-1b82-4fd8-933a-13a46b2dc003' , 0);
-
+			VALUES (1, '1' , 1);
+			
+-- Sequences: Deve conter o próximo id a ser utilizado 
 INSERT INTO HIBERNATE_SEQUENCES (SEQUENCE_NAME, SEQUENCE_NEXT_HI_VALUE ) 
-			VALUES  ('ConfiguracaoSistema', 2);
+			VALUES  ('ConfiguracaoSistema', 1000);
 
+---------------------------------------------------------------------------------------------
+-- Criação de Menus 
+---------------------------------------------------------------------------------------------
+
+-- Projeto			
 INSERT INTO FUNCIONALIDADE(
-            id, uuid, version, nome, srcjanela, funcionalidadepai_id, configuracoes_sistema_id, 
-            pos, funcionalidade_id) 
+            id, uuid, version, nome, srcjanela, funcionalidadepai_id, configuracoes_sistema_id, pos, funcionalidade_id) 
             VALUES (1, '1', 1, 'Projetos', '', null, 1, 0, null);
             
+-- Cadastrar Projetos          
 INSERT INTO FUNCIONALIDADE(
-            id, uuid, version, nome, srcjanela, funcionalidadepai_id, configuracoes_sistema_id, 
-            pos, funcionalidade_id) 
-            VALUES (2, '2', 2, 'Cadastrar Projetos', 'ode.controleProjeto.cci.CtrlProjetoCRUD', 1, null, 0, 1);
+            id, uuid, version, nome, srcjanela, funcionalidadepai_id, configuracoes_sistema_id, pos, funcionalidade_id) 
+            VALUES (11, '11', 11, 'Cadastrar Projetos', 'ode.controleProjeto.cci.CtrlProjetoCRUD', 1, null, 0, 1);
 
+-- Selecionar Projetos            
 INSERT INTO FUNCIONALIDADE(
-            id, uuid, version, nome, srcjanela, funcionalidadepai_id, configuracoes_sistema_id, 
-            pos, funcionalidade_id) 
-            VALUES (3, '3', 3, 'Selecionar Projeto', 'ode.controleProjeto.cci.CtrlSelecionarProjeto', 1, null, 1, 1);
+            id, uuid, version, nome, srcjanela, funcionalidadepai_id, configuracoes_sistema_id, pos, funcionalidade_id) 
+            VALUES (12, '12', 12, 'Selecionar Projeto', 'ode.controleProjeto.cci.CtrlSelecionarProjeto', 1, null, 1, 1);
+
+-- Administracao 
+INSERT INTO FUNCIONALIDADE(
+            id, uuid, version, nome, srcjanela, funcionalidadepai_id, configuracoes_sistema_id, pos, funcionalidade_id) 
+            VALUES (2, '2', 2, 'Administração', '', null, 1, 1, null);
+-- Usuarios
+INSERT INTO FUNCIONALIDADE(
+            id, uuid, version, nome, srcjanela, funcionalidadepai_id, configuracoes_sistema_id, pos, funcionalidade_id) 
+            VALUES (21, '21', 21, 'Usuários', 'ode.controleUsuario.cci.UsuarioCtrlCRUD', 2, null, 0, 2);
+
+-- Menu Recursos
+INSERT INTO FUNCIONALIDADE(
+            id, uuid, version, nome, srcjanela, funcionalidadepai_id, configuracoes_sistema_id, pos, funcionalidade_id) 
+            VALUES (3, '3', 3, 'Recursos', '', null, 1, 2, null);
+
+-- Recursos Humanos            
+INSERT INTO FUNCIONALIDADE(
+            id, uuid, version, nome, srcjanela, funcionalidadepai_id, configuracoes_sistema_id, pos, funcionalidade_id) 
+            VALUES (31, '31', 31, 'Recursos Humanos', 'ode.controleProcesso.cci.CtrlCRUDRecursoHumano', 3, null, 0, 3);
             
+-- Sequences: Deve conter o próximo id a ser utilizado            
 INSERT INTO HIBERNATE_SEQUENCES (SEQUENCE_NAME, SEQUENCE_NEXT_HI_VALUE ) 
-			VALUES  ('Funcionalidade', 4);
-            
+			VALUES  ('Funcionalidade', 1000);
+
+---------------------------------------------------------------------------------------------
+-- Perfis de Acesso: Cada novo perfil deve ser criado aqui.
+---------------------------------------------------------------------------------------------
+
+-- Administrador
 INSERT INTO PERFILACESSO(
             id, uuid, version, nome)
-    		VALUES (1 , '9010512e-ba0a-444a-83cb-048698e98404' , 4 , 'Administrador');
-    		
-INSERT INTO HIBERNATE_SEQUENCES (SEQUENCE_NAME, SEQUENCE_NEXT_HI_VALUE ) 
-			VALUES  ('PerfilAcesso', 2);
-    		
+    		VALUES (1 , '1' , 1 , 'Administrador');
+
 INSERT INTO PERFILACESSO_FUNCIONALIDADE(
             perfilacesso_id, funcionalidadespermitidas_id)
     		VALUES (1, 1);
 
 INSERT INTO PERFILACESSO_FUNCIONALIDADE(
             perfilacesso_id, funcionalidadespermitidas_id)
-    		VALUES (1, 2);
+    		VALUES (1, 11);
+    		
+INSERT INTO PERFILACESSO_FUNCIONALIDADE(
+            perfilacesso_id, funcionalidadespermitidas_id)
+    		VALUES (1, 12);
  
+INSERT INTO PERFILACESSO_FUNCIONALIDADE(
+            perfilacesso_id, funcionalidadespermitidas_id)
+    		VALUES (1, 2);
+    		
+INSERT INTO PERFILACESSO_FUNCIONALIDADE(
+            perfilacesso_id, funcionalidadespermitidas_id)
+    		VALUES (1, 21);
+    		
 INSERT INTO PERFILACESSO_FUNCIONALIDADE(
             perfilacesso_id, funcionalidadespermitidas_id)
     		VALUES (1, 3);
     		
-INSERT INTO RECURSOHUMANO (id, uuid, version, nome, ativo)
-VALUES (1, '9440682b-b0e8-4341-9179-bc623b89d083', 0, 'Ricardo de Almeida Falbo', true);
-INSERT INTO RECURSOHUMANO (id, uuid, version, nome, ativo)
-VALUES (2, '6b42720e-5b31-4971-a055-e244430d957f', 0, 'Alexandre Coelho', true);
+INSERT INTO PERFILACESSO_FUNCIONALIDADE(
+            perfilacesso_id, funcionalidadespermitidas_id)
+    		VALUES (1, 31);
+    		
+-- Desenvolvedor
+INSERT INTO PERFILACESSO(
+            id, uuid, version, nome)
+    		VALUES (2 , '2' , 2 , 'Desenvolvedor');
+    		
+INSERT INTO PERFILACESSO_FUNCIONALIDADE(
+            perfilacesso_id, funcionalidadespermitidas_id)
+    		VALUES (2, 1);
+
+INSERT INTO PERFILACESSO_FUNCIONALIDADE(
+            perfilacesso_id, funcionalidadespermitidas_id)
+    		VALUES (2, 12);
+    		
+-- Sequences: Deve conter o próximo id a ser utilizado     		
 INSERT INTO HIBERNATE_SEQUENCES (SEQUENCE_NAME, SEQUENCE_NEXT_HI_VALUE ) 
-VALUES  ('RECURSOHUMANO', 1);
+			VALUES  ('PerfilAcesso', 1000);
+
+---------------------------------------------------------------------------------------------
+-- Recursos Humanos 
+---------------------------------------------------------------------------------------------  		
+    		
+INSERT INTO RECURSOHUMANO (id, uuid, version, nome, ativo)
+VALUES (1, '1', 1, 'Ricardo de Almeida Falbo', true);
+
+INSERT INTO RECURSOHUMANO (id, uuid, version, nome, ativo)
+VALUES (2, '2', 2, 'Alexandre G. N. Coelho', true);
+
+-- Sequences: Deve conter o próximo id a ser utilizado    
+INSERT INTO HIBERNATE_SEQUENCES (SEQUENCE_NAME, SEQUENCE_NEXT_HI_VALUE ) 
+VALUES  ('RecursoHumano', 1000);
+
+---------------------------------------------------------------------------------------------
+-- Usuários
+---------------------------------------------------------------------------------------------
 
 INSERT INTO NUCLEOUSERDETAILS (id, uuid, version, username, password, perfilacesso_id, recursohumano_id)
 VALUES (1, '36ac7738-b3bc-40cc-98ab-21c5f59db410', 0, 'admin', 'e8d95a51f3af4a3b134bf6bb680a213a',1, 1);
+
 INSERT INTO NUCLEOUSERDETAILS (id, uuid, version, username, password, perfilacesso_id, recursohumano_id)
-VALUES (2, '45as7738-b3bc-40cc-98ab-21c5f59db410', 0, 'alexandre.coelho', 'e8d95a51f3af4a3b134bf6bb680a213a', 1, 2);
+VALUES (2, '45as7738-b3bc-40cc-98ab-21c5f59db410', 0, 'alexandre.coelho', 'e8d95a51f3af4a3b134bf6bb680a213a', 2, 2);
+
 INSERT INTO HIBERNATE_SEQUENCES (SEQUENCE_NAME, SEQUENCE_NEXT_HI_VALUE ) 
 VALUES  ('NucleoUserDetails', 1);
+
+---------------------------------------------------------------------------------------------
+-- Granted Authority : Utilizada na autenticação de users. Não mexer.
+---------------------------------------------------------------------------------------------
+
 INSERT INTO GRANTEDAUTHORITYIMPL (id, uuid, version, authority)
-VALUES (1, '07d679c5-fe3b-411c-9d5b-1e2a343f76d7', 0, 'ROLE_SUPERVISOR');
+VALUES (1, '1', 1, 'ROLE_SUPERVISOR');
+
 INSERT INTO GRANTEDAUTHORITYIMPL (id, uuid, version, authority)
-VALUES (2, '07d679c5-fe3b-411c-9d5b-1e2a343f76d7', 0, 'ROLE_SUPERVISOR');
+VALUES (2, '2', 2, 'ROLE_SUPERVISOR');
+
 INSERT INTO HIBERNATE_SEQUENCES (SEQUENCE_NAME, SEQUENCE_NEXT_HI_VALUE ) 
-VALUES  ('NucleoGrantedAuthority', 1);
+VALUES  ('GrantedAuthorityImpl', 1000);
+
+---------------------------------------------------------------------------------------------
+-- Permitir que o usuários desse gerados nesse script possam autenticar no ODE 
+---------------------------------------------------------------------------------------------
+
 INSERT INTO NUCLEOUSERDETAILS_GRANTEDAUTHORITYIMPL
 VALUES (1,1);
 INSERT INTO NUCLEOUSERDETAILS_GRANTEDAUTHORITYIMPL 
 VALUES (2,2);
+
+---------------------------------------------------------------------------------------------
+-- KTipoInteracao
+---------------------------------------------------------------------------------------------
 
 INSERT INTO KTIPOINTERACAO (id, uuid, version, nome, descricao) VALUES (1, '65as7738-b3bc-40cc-98ab-21c5f59db410', 0, 'Sequencial', 'Tipo de Interacao Sequencial');
 INSERT INTO KTIPOINTERACAO (id, uuid, version, nome, descricao) VALUES (2, '65as7738-b3bc-40cc-98ab-21c5f59db411', 0, 'Paralelo Independente', 'Tipo de Interacao Paralelo Independente');
 INSERT INTO KTIPOINTERACAO (id, uuid, version, nome, descricao) VALUES (3, '65as7738-b3bc-40cc-98ab-21c5f59db412', 0, 'Paralelo Dependente', 'Tipo de Interacao Paralelo Dependente');
 INSERT INTO KTIPOINTERACAO (id, uuid, version, nome, descricao) VALUES (4, '65as7738-b3bc-40cc-98ab-21c5f59db413', 0, 'Pontual', 'Tipo de Interacao Pontual');
 INSERT INTO KTIPOINTERACAO (id, uuid, version, nome, descricao) VALUES (5, '65as7738-b3bc-40cc-98ab-21c5f59db414', 0, 'Sob-Demanda', 'Tipo de Interacao Sob-Demanda');
+
+---------------------------------------------------------------------------------------------
+-- KCategoriaProcesso
+---------------------------------------------------------------------------------------------
 
 INSERT INTO KCATEGORIAPROCESSO (id, uuid, version, nome, descricao) VALUES (6, '65as7738-b3bc-40cc-98ab-21c5f59db415', 0, 'Fundamental', 'Categoria Fundamental');
 INSERT INTO KCATEGORIAPROCESSO (id, uuid, version, nome, descricao) VALUES (7, '65as7738-b3bc-40cc-98ab-21c5f59db416', 0, 'Apoio', 'Categoria de Apoio');
