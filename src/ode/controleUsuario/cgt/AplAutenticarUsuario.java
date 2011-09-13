adff390f71bcd60a5a6739f310405b681663bcac
renato.g.chaves <renato.g.chaves@gmail.com>
2011-09-13 19:34:53 +0000

    Comentários nos arquivos que fazem autenticação de usuários

--- src/ode/controleUsuario/cgt/AplAutenticarUsuario.java
+++ src/ode/controleUsuario/cgt/AplAutenticarUsuario.java
@@ -8,12 +8,35 @@ import ode.nucleo.excecao.NucleoRegraNegocioExcecao;
 
 public interface AplAutenticarUsuario {
 
+	/*
+	 * Efetua o login de um usuário no sistema, possivelmente salvando um cookie para preservar a identificação para visitas posteriores.
+	 * 
+	 * @param nomeUsuario
+	 * Nome do usuário, conforme informado na interface
+	 * 
+	 * @param senha
+	 * Senha do usuário, conforme informada na interface
+	 * 
+	 * @param rememberMe
+	 * Boolean informando se o usuário desejar permanecer conectado
+	 */
 	public void efetuarLogin(String nomeUsuario, String senha, boolean rememberme) throws NucleoRegraNegocioExcecao;
 	
+	/*
+	 * Remove o usuário da sessão e exclui os devidos cookies do navegador
+	 */
 	public void efetuarLogout();
 	
+	/*
+	 * Utiliza os cookies do navegador para identificar o usuário no sistema
+	 * @return Se o usuário foi ou não logado no sistema
+	 */
 	public boolean recuperarLoginCookie(String nomeUsuario, String token);
 	
+	/*
+	 * Obtém as funcionalidades do sistema e suas permissões
+	 * @return Lista de funcionalidades 
+	 */
 	public List<Funcionalidade> obterFuncionalidades();
 	
 }
