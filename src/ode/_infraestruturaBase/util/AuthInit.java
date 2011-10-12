54512c8395f34123a97897d6dc9ab486156c223e
alexandregncoelho <alexandregncoelho@gmail.com>
2011-10-12 18:10:40 +0000

    Alteração do redirecionamento da página login.zul e inserção da pasta build.

--- src/ode/_infraestruturaBase/util/AuthInit.java
+++ src/ode/_infraestruturaBase/util/AuthInit.java
@@ -2,7 +2,6 @@ package ode._infraestruturaBase.util;
 
 import java.util.Map;
 
-import javax.servlet.ServletContext;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -12,7 +11,6 @@ import ode.controleUsuario.cgt.AplAutenticarUsuario;
 import org.zkoss.zk.ui.Execution;
 import org.zkoss.zk.ui.Executions;
 import org.zkoss.zk.ui.Page;
-import org.zkoss.zk.ui.Sessions;
 import org.zkoss.zk.ui.util.Initiator;
 import org.zkoss.zkplus.spring.SpringUtil;
 
@@ -44,10 +42,10 @@ public class AuthInit implements Initiator {
 			//cookies inexistentes ou invalidos -> redirecionar para login
 			if (!autenticado) {
 				
-				ServletContext sc = (ServletContext)Sessions.getCurrent().getWebApp().getNativeContext();
-				String fullPath = sc.getRealPath("login.zul");
+//				ServletContext sc = (ServletContext)Sessions.getCurrent().getWebApp().getNativeContext();
+//				String fullPath = sc.getRealPath("login.zul");
 				
-				((HttpServletResponse) exec.getNativeResponse()).sendRedirect(fullPath);
+				((HttpServletResponse) exec.getNativeResponse()).sendRedirect("login.zul");
 				exec.setVoided(true);
 			}
 		}
