d65bf1e91b50657a239d3978f917eefcb4199de9
rsjuliao@gmail.com <rsjuliao@gmail.com@9a94fb9e-9367-8fd8-e54f-9d0b81a662fe>
2011-10-27 19:42:30 +0000

    inclusao das classes que faltaram

---
+++ src/ode/processoPadrao/cgd/EstruturaCompPPDAOImpl.java
@@ -0,0 +1,11 @@
+package ode.processoPadrao.cgd;
+
+import org.springframework.stereotype.Repository;
+
+import ode._infraestruturaBase.cgd.DAOBaseImpl;
+import ode.processoPadrao.cdp.EstruturaCompPP;
+
+@Repository
+public class EstruturaCompPPDAOImpl extends DAOBaseImpl<EstruturaCompPP> implements EstruturaCompPPDAO{
+
+}
