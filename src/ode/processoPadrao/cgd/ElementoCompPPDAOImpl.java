45e2af78213610f3dae425911c4f0ecb08a318d8
rsjuliao <rsjuliao@gmail.com>
2011-10-27 19:42:30 +0000

    inclusao das classes que faltaram

---
+++ src/ode/processoPadrao/cgd/ElementoCompPPDAOImpl.java
@@ -0,0 +1,11 @@
+package ode.processoPadrao.cgd;
+
+import org.springframework.stereotype.Repository;
+
+import ode._infraestruturaBase.cgd.DAOBaseImpl;
+import ode.processoPadrao.cdp.ElementoCompPP;
+
+@Repository
+public class ElementoCompPPDAOImpl extends DAOBaseImpl<ElementoCompPP> implements ElementoCompPPDAO{
+
+}
