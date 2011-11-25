587cd8501ac5a079a04becf612e46e1766b12db8
renato.g.chaves <renato.g.chaves@gmail.com>
2011-11-25 18:51:41 +0000

    Alteração que permite definir as larguras das colunas, adicionar string no campo da direita sem ter que criar label e adicionar linha com um único elemento.

--- src/ode/_infraestruturaCRUD/ciu/GridDados.java
+++ src/ode/_infraestruturaCRUD/ciu/GridDados.java
@@ -1,6 +1,9 @@
 package ode._infraestruturaCRUD.ciu;
 
 import org.zkoss.zk.ui.Component;
+import org.zkoss.zul.Cell;
+import org.zkoss.zul.Column;
+import org.zkoss.zul.Columns;
 import org.zkoss.zul.Grid;
 import org.zkoss.zul.Label;
 import org.zkoss.zul.Row;
@@ -12,6 +15,10 @@ public class GridDados extends Grid {
 	 * 
 	 */
 	private static final long serialVersionUID = -7133443852058334459L;
+	
+	private Columns colunas = new Columns();
+	private Column colunaEsquerda = new Column();
+	private Column colunaDireita = new Column();
 	private Rows linhas = new Rows();
 
 	public GridDados() {
@@ -20,8 +27,26 @@ public class GridDados extends Grid {
 	}
 
 	protected void configurarComponentes() {
+		colunaEsquerda.setWidth("50%");
+		colunaEsquerda.setParent(colunas);
+		
+		colunaDireita.setWidth("50%");
+		colunaDireita.setParent(colunas);
+		
+		colunas.setVisible(false);
+		
+		colunas.setParent(this);
 		linhas.setParent(this);
 	}
+	
+	public void setLarguras(String largura1, String largura2) {
+		colunas.getChildren().clear();
+		colunaEsquerda.setWidth(largura1);
+		colunaEsquerda.setParent(colunas);
+		colunaDireita.setWidth(largura2);
+		colunaDireita.setParent(colunas);	
+	}
+	
 /**
  * Método que adiciona uma linha simples ao grid de dados
  * */
@@ -35,6 +60,17 @@ public class GridDados extends Grid {
 
 	}
 	
+	public void adicionarLinha(String titulo, String valor) {
+		Row linha = new Row();
+		linha.setParent(linhas);
+		
+		Label label = new Label(titulo + ": ");
+		label.setParent(linha);
+		
+		Label label2 = new Label(valor);
+		label2.setParent(linha);
+	}
+	
 	
 	public void adicionarLinhaObrigatoria(String tituloLabel, Component elemento) {
 		Row linha = new Row();
@@ -45,6 +81,43 @@ public class GridDados extends Grid {
 		label.setParent(linha);
 		elemento.setParent(linha);
 
+	}
+
+	public void adicionarLinhaDupla(String labelTitulo, Component elemento) {
+		Row linha = new Row();
+		linha.setParent(linhas);
+		
+		Cell cell = new Cell();
+		cell.setColspan(2);
+		cell.setParent(linha);
+		
+		Label label = new Label(labelTitulo+ ": ");
+		label.setParent(cell);
+		
+		elemento.setParent(cell);
+	}
+	
+	public void adicionarLinhaUnica(String str) {
+		Row linha = new Row();
+		linha.setParent(linhas);
+		
+		Cell cell = new Cell();
+		cell.setColspan(2);
+		cell.setParent(linha);
+		
+		Label label = new Label(str);
+		label.setParent(cell);
+	}
+	
+	public void adicionarLinhaUnica(Component elemento) {
+		Row linha = new Row();
+		linha.setParent(linhas);
+		
+		Cell cell = new Cell();
+		cell.setColspan(2);
+		cell.setParent(linha);
+		
+		elemento.setParent(cell);
 	}
 
 }
