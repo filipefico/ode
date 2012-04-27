de0ad6512edf6f2b443b8c8030d582d7c7f7de88
alexandregncoelho@gmail.com <alexandregncoelho@gmail.com@9a94fb9e-9367-8fd8-e54f-9d0b81a662fe>
2012-04-27 21:05:32 +0000

    modificacoes na gerencia de conhecimento.

--- src/ode/gerenciaConhecimento/cdp/ConhecimentoRelativoDiscussao.java
+++ src/ode/gerenciaConhecimento/cdp/ConhecimentoRelativoDiscussao.java
@@ -4,7 +4,7 @@ import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
 
 @Entity
-@DiscriminatorValue("Conhecimento Relativo a Discussao")
+@DiscriminatorValue("Relativo a Discussao")
 public class ConhecimentoRelativoDiscussao extends ItemConhecimento {
 
 
