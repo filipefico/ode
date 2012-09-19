6763e0a7723b3cd70e95213c8c0b63639c2dcb67
caducbraga <caducbraga@9a94fb9e-9367-8fd8-e54f-9d0b81a662fe>
2012-09-19 22:58:03 +0000

    Alterações em ReqODE.

--- src/ode/gerenciaRequisitos/cih/PainelVisualizarRequisito.java
+++ src/ode/gerenciaRequisitos/cih/PainelVisualizarRequisito.java
@@ -1,18 +1,22 @@
 package ode.gerenciaRequisitos.cih;
 
-import java.util.Collection;
+import java.text.SimpleDateFormat;
+import java.util.Set;
 
 import ode._controleRecursoHumano.cdp.RecursoHumano;
 import ode._infraestruturaCRUD.ciu.GridDados;
+import ode.conhecimento.principal.cdp.Conhecimento;
 import ode.gerenciaRequisitos.cdp.Requisito;
 import ode.uml.cdp.CasoUso;
+import ode.uml.cdp.Classe;
 import ode.uml.cdp.Pacote;
 
-import org.zkoss.zul.Datebox;
+import org.zkoss.zul.Label;
 import org.zkoss.zul.Listbox;
 import org.zkoss.zul.Listcell;
+import org.zkoss.zul.Listhead;
+import org.zkoss.zul.Listheader;
 import org.zkoss.zul.Listitem;
-import org.zkoss.zul.Textbox;
 import org.zkoss.zul.Vlayout;
 
 public class PainelVisualizarRequisito extends Vlayout {
@@ -30,110 +34,187 @@ public class PainelVisualizarRequisito extends Vlayout {
 		GridDados grid = new GridDados();
 		grid.setParent(this);
 
-		Textbox tbIdentificador = new Textbox(requisito.getIdentificador());
-		tbIdentificador.setReadonly(true);
-		grid.adicionarLinha("Identificador", tbIdentificador);
+		grid.adicionarLinha("Identificador",
+				new Label(requisito.getIdentificador()));
 
-		Datebox dbCriacao = new Datebox(requisito.getDataCriacao());
-		dbCriacao.setReadonly(true);
-		grid.adicionarLinha("Data de Criação", dbCriacao);
+		grid.adicionarLinha("Descrição", new Label(requisito.getDescricao()));
 
-		Textbox tbPrioridade = new Textbox(requisito.getPrioridade().getNome());
-		tbPrioridade.setReadonly(true);
-		grid.adicionarLinha("Prioridade", tbPrioridade);
+		grid.adicionarLinha(
+				"Data de Criação",
+				new Label(new SimpleDateFormat().format(requisito
+						.getDataCriacao())));
 
-		Textbox tbDescricao = new Textbox(requisito.getDescricao());
-		tbDescricao.setRows(5);
-		tbDescricao.setMultiline(true);
-		tbDescricao.setReadonly(true);
-		grid.adicionarLinha("Descrição", tbDescricao);
+		grid.adicionarLinha("Prioridade", new Label(requisito.getPrioridade()
+				.name()));
 
-		Textbox tbProjeto = new Textbox(requisito.getProjeto().getNome());
-		tbProjeto.setReadonly(true);
-		grid.adicionarLinha("Projeto", tbProjeto);
+		grid.adicionarLinha("Tipo de Requisito", new Label(requisito
+				.getTipoRequisito().getNome()));
 
-		Textbox tbTipoRequisito = new Textbox(requisito.getTipoRequisito()
-				.getNome());
-		tbTipoRequisito.setReadonly(true);
-		grid.adicionarLinha("Tipo", tbTipoRequisito);
+		grid.adicionarLinha("Categoria de Requisito",
+				requisito.getCategoria() != null ? new Label(requisito
+						.getCategoria().getNome()) : new Label("<<Vazio>>"));
 
-		Textbox tbCategoria = new Textbox(requisito.getCategoria().getNome());
-		tbCategoria.setReadonly(true);
-		grid.adicionarLinha("Categoria", tbCategoria);
+		Listbox listbox = new Listbox();
 
-		Listbox lbConflitos = new Listbox();
-		Collection<Requisito> conflitos = requisito.getConflitos();
-		for (Requisito r : conflitos) {
+		Listhead listhead = new Listhead();
+		listhead.setParent(listbox);
+
+		new Listheader("ID").setParent(listhead);
+		new Listheader("Descrição").setParent(listhead);
+
+		Set<Requisito> dependencias = requisito.getDependencias();
+
+		for (Requisito requisito2 : dependencias) {
 			Listitem item = new Listitem();
-			new Listcell(r.getIdentificador()).setParent(item);
-			new Listcell(r.getDescricao().substring(0, 99) + "...")
-					.setParent(item);
 
-			item.setValue(r);
-			item.setParent(lbConflitos);
+			new Listcell(requisito2.getIdentificador()).setParent(item);
+			new Listcell(requisito2.getDescricao()).setParent(item);
+
+			item.setParent(listbox);
 		}
-		grid.adicionarLinha("Conflitos", lbConflitos);
+		if (dependencias.size() == 0) {
+			grid.adicionarLinha("Dependências", new Label("<<Vazio>>"));
+		} else {
+			grid.adicionarLinha("Dependências", listbox);
+		}
 
-		Listbox lbDependencias = new Listbox();
-		Collection<Requisito> dependencias = requisito.getDependencias();
-		for (Requisito r : dependencias) {
+		listbox = new Listbox();
+
+		listhead = new Listhead();
+		listhead.setParent(listbox);
+
+		new Listheader("ID").setParent(listhead);
+		new Listheader("Descrição").setParent(listhead);
+
+		Set<Requisito> conflitos = requisito.getConflitos();
+		for (Requisito requisito2 : conflitos) {
 			Listitem item = new Listitem();
-			new Listcell(r.getIdentificador()).setParent(item);
-			new Listcell(r.getDescricao().substring(0, 99) + "...")
-					.setParent(item);
 
-			item.setValue(r);
-			item.setParent(lbDependencias);
+			new Listcell(requisito2.getIdentificador()).setParent(item);
+			new Listcell(requisito2.getDescricao()).setParent(item);
+
+			item.setParent(listbox);
 		}
-		grid.adicionarLinha("Dependências", lbDependencias);
+		if (conflitos.size() == 0) {
+			grid.adicionarLinha("Conflitos", new Label("<<Vazio>>"));
+		} else {
+			grid.adicionarLinha("Conflitos", listbox);
+		}
 
-		Listbox lbPacotes = new Listbox();
-		Collection<Pacote> pacotes = requisito.getPacotes();
-		for (Pacote p : pacotes) {
+		listbox = new Listbox();
+
+		listhead = new Listhead();
+		listhead.setParent(listbox);
+
+		new Listheader("Nome").setParent(listhead);
+
+		Set<RecursoHumano> interessados = requisito.getInteressados();
+		for (RecursoHumano recursoHumano : interessados) {
 			Listitem item = new Listitem();
-			new Listcell(p.getNome()).setParent(item);
-			new Listcell(p.getDescricao().substring(0, 99) + "...")
-					.setParent(item);
 
-			item.setValue(p);
-			item.setParent(lbPacotes);
+			new Listcell(recursoHumano.getNome()).setParent(item);
+
+			item.setParent(listbox);
 		}
-		grid.adicionarLinha("Pacotes", lbPacotes);
+		if (interessados.size() == 0) {
+			grid.adicionarLinha("Interessados", new Label("<<Vazio>>"));
+		} else {
+			grid.adicionarLinha("Interessados", listbox);
+		}
 
-		Listbox lbCasosUso = new Listbox();
-		Collection<CasoUso> casosUso = requisito.getCasosUso();
-		for (CasoUso c : casosUso) {
+		listbox = new Listbox();
+
+		listhead = new Listhead();
+		listhead.setParent(listbox);
+
+		new Listheader("Nome").setParent(listhead);
+
+		Set<RecursoHumano> responsaveis = requisito.getResponsaveis();
+		for (RecursoHumano recursoHumano : responsaveis) {
 			Listitem item = new Listitem();
-			new Listcell(c.getNome()).setParent(item);
-			new Listcell(c.getDescricao().substring(0, 99) + "...")
-					.setParent(item);
 
-			item.setValue(c);
-			item.setParent(lbCasosUso);
+			new Listcell(recursoHumano.getNome()).setParent(item);
+
+			item.setParent(listbox);
 		}
-		grid.adicionarLinha("Casos de Uso", lbCasosUso);
+		if (responsaveis.size() == 0) {
+			grid.adicionarLinha("Responsáveis", new Label("<<Vazio>>"));
+		} else {
+			grid.adicionarLinha("Responsáveis", listbox);
+		}
 
-		Listbox lbResponsaveis = new Listbox();
-		Collection<RecursoHumano> responsaveis = requisito.getResponsaveis();
-		for (RecursoHumano resp : responsaveis) {
+		listbox = new Listbox();
+
+		listhead = new Listhead();
+		listhead.setParent(listbox);
+
+		new Listheader("Nome").setParent(listhead);
+		new Listheader("Descrição").setParent(listhead);
+
+		Set<Pacote> pacotes = requisito.getPacotes();
+		for (Conhecimento conhecimento : pacotes) {
 			Listitem item = new Listitem();
-			new Listcell(resp.getNome()).setParent(item);
 
-			item.setValue(resp);
-			item.setParent(lbResponsaveis);
+			new Listcell(conhecimento.getNome()).setParent(item);
+			new Listcell(conhecimento.getDescricao()).setParent(item);
+
+			item.setParent(listbox);
 		}
-		grid.adicionarLinha("Responsáveis", lbResponsaveis);
+		if (pacotes.size() == 0) {
+			grid.adicionarLinha("Pacotes Relacionados", new Label("<<Vazio>>"));
+		} else {
+			grid.adicionarLinha("Pacotes Relacionados", listbox);
+		}
 
-		Listbox lbInteressados = new Listbox();
-		Collection<RecursoHumano> interessados = requisito.getInteressados();
-		for (RecursoHumano interess : interessados) {
+		listbox = new Listbox();
+
+		listhead = new Listhead();
+		listhead.setParent(listbox);
+
+		new Listheader("Nome").setParent(listhead);
+		new Listheader("Descrição").setParent(listhead);
+
+		Set<CasoUso> casosUso = requisito.getCasosUso();
+		for (Conhecimento conhecimento : casosUso) {
 			Listitem item = new Listitem();
-			new Listcell(interess.getNome()).setParent(item);
 
-			item.setValue(interess);
-			item.setParent(lbInteressados);
+			new Listcell(conhecimento.getNome()).setParent(item);
+			new Listcell(conhecimento.getDescricao()).setParent(item);
+
+			item.setParent(listbox);
 		}
-		grid.adicionarLinha("Interessados", lbInteressados);
+		if (casosUso.size() == 0) {
+			grid.adicionarLinha("Casos de Uso Relacionados", new Label(
+					"<<Vazio>>"));
+		} else {
+			grid.adicionarLinha("Casos de Uso Relacionados", listbox);
+		}
+
+		listbox = new Listbox();
+
+		listhead = new Listhead();
+		listhead.setParent(listbox);
+
+		new Listheader("Nome").setParent(listhead);
+		new Listheader("Descrição").setParent(listhead);
+
+		casosUso = requisito.getCasosUso();
+		for (CasoUso casoUso : casosUso) {
+			Set<Classe> classes = casoUso.getClasses();
+			for (Classe classe : classes) {
+				Listitem item = new Listitem();
+
+				new Listcell(classe.getNome()).setParent(item);
+				new Listcell(classe.getDescricao()).setParent(item);
+
+				item.setParent(listbox);
+			}
+		}
+		if (listbox.getItems().size() == 0) {
+			grid.adicionarLinha("Classes Relacionadas", new Label("<<Vazio>>"));
+		} else {
+			grid.adicionarLinha("Classes Relacionadas", listbox);
+		}
 
 	}
 }
